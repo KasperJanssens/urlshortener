d53b4fcbdc8c7a183540a5bd36e2dd6b5ee50456
Kasper Janssens <kasper@krondorsoft.com>
2021-10-15 15:13:34 +0200

    Add Design png and update script

--- runShorten.sh
+++ runShorten.sh
@@ -7,8 +7,13 @@ else
   echo "Curl not found"
   exit 1
 fi
+if [ -z "$1" ]
+  then
+    echo "url to shorten not provided as an argument"
+    exit 1
+fi
 
 set -ex
 
-curl -X POST -H "Content-Type: application/json" -v -d "{\"url\": \"joske\"}" localhost:8080/shorten
+curl -X POST -H "Content-Type: application/json" -v -d "{\"url\": \"$1\"}" localhost:8080/shorten
 
